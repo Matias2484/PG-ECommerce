585e4d1bedd5eab505777eb85381a971f2600351
Ignacio Romanwow <romanowignacio1@gmail.com>
2021-08-23 21:37:56 -0300

    add(logginFixed-googleRegister)

--- client/src/components/recoverPass/recoverPopUp.jsx
+++ client/src/components/recoverPass/recoverPopUp.jsx
@@ -1,20 +1,52 @@
-import React, { useState }  from 'react'
+import React, { useState } from 'react'
 import ReactCircleModal from 'react-circle-modal'
 import   './recoverPopUp.css'
-import validarEmail from '../../funciones/validateEmail'
-import { useDispatch } from 'react-redux'
+import swal from 'sweetalert';
 
 
-    export default function RecoverPopUp (){
-        
+
+export default function RecoverPopUp (){
+    const [aprove, setAprove] = useState(false)
+    
+
+const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function validarEmail(e) {
+    const valor = e.target.value
+    
+    let msgRecoverPass = document.getElementById('msgRecoverPass')
+    let btnRecoverPass = document.getElementById('enviaPop')
+    if (re.test(String(valor).toLowerCase())){
+        setAprove(true) 
+        console.log(aprove)
+        btnRecoverPass.style.backgroundColor = 'rgb(244, 164, 96)'
+        msgRecoverPass.style.color = 'green'
+        msgRecoverPass.textContent = "La dirección de email " +  valor + " es correcta!."
+    } else {
+        setAprove(false) 
+        console.log(aprove)
+        btnRecoverPass.style.backgroundColor = 'grey'
+        msgRecoverPass.style.color = 'red'
+        msgRecoverPass.textContent = "La dirección de email es incorrecta!."
+    }
+}   
+
+
+    function handleSumbit(){
+        document.getElementById('')
+        swal({
+            title: "Enviado",
+            text: "revisa tu casilla de entrada o de spam recibiras un mail con una nueva contraseña",
+            icon: "success",
+            });
+        }
+
 
     return (
         <ReactCircleModal 
-        
         backgroundColor="rgb(244, 164, 96)"
         toogleComponent={onClick => (
             <button className="openPop" onClick={onClick}>
-            Recuperar contraseña
+            ¡Recupérala!
             </button>
         )}
         offsetX={0}
@@ -24,21 +56,26 @@ import { useDispatch } from 'react-redux'
             
             <div  className="pepe">
             
-            <p>escribe tu direccion de correo electronico a continuacion y recibiras una nueva clave en la bandeja de entrada</p>
-
+            <p>Escribe tu direccion de correo electronico a continuacion y recibiras una nueva clave</p><br />
+            <p> revisa la bandeja de entrada o tu casilla de spam </p>
+            <span id="msgRecoverPass"></span>
             <input 
             className="inputMail" 
             type="text" 
             placeholder="Escribe tu email..."
             name="email"
             changui="true"
+            onChange={validarEmail}
             />
-            <button className="enviaPop" >enviar</button>
+            {aprove ? (<button id="enviaPop" onClick={handleSumbit}>Enviar</button>):(
+                <button id="enviaPop">nup :'C</button>
+            )}
+            
             <button className="closePop" onClick={onClick}>cancelar</button>
             </div>
         )}
         </ReactCircleModal>
     )
     }
-
+    
 
--- client/src/components/registerForm/registerForm.jsx
+++ client/src/components/registerForm/registerForm.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
 import swal from 'sweetalert';
 import newUser from '../../funciones/newUser'
+import '../registerForm/registerForm.css'
+import GoogleLogin from 'react-google-login';
+import { useDispatch } from 'react-redux';
 
-export default function RegisterForm({isOpen, closeModal}){
+export default function RegisterForm({loginBarFunction}){
+    const dispatch = useDispatch()
     const [input, setInput] = useState({
         nombre: '',
         apellido:'',
@@ -13,41 +16,76 @@ export default function RegisterForm({isOpen, closeModal}){
         admin: true,
         });
         
-        const dispatch = useDispatch()
-        const handleInputChange = (e) => {
-    
-            setInput({
-              ...input,  //Trae el estado anterior, para que no se pise con el nuevo estado.
-                [e.target.name]: e.target.value
+        
+    const handleInputChange = (e) => {
+
+        setInput({
+            ...input,  //Trae el estado anterior, para que no se pise con el nuevo estado.
+            [e.target.name]: e.target.value
+        });
+    }
+    const enviarInput = (e) => {
+        e.preventDefault();
+        loginBarFunction();
+        closeRegisModal();
+        if(input.nombre && input.password && input.email) {
+            swal({
+            title: "Gracias",
+            text: "Formulario enviado con éxito",
+            icon: "success",
             });
+            newUser(input);
         }
-        const enviarInput = (e) => {
-            e.preventDefault();
-            if(input.nombre && input.password && input.email) {
-                swal({
-                title: "Gracias",
-                text: "Formulario enviado con éxito",
-                icon: "success",
-                });
-                newUser(input);
-            }
-            }
+        }
+
+    
+        const closeRegisModal = () => {
+            let logModal = document.getElementById('regisModal')
+            let ninjaButton = document.getElementById('buttonsForms')
+            logModal.style.opacity = '0';
+            logModal.style.zIndex = '1'; 
+            ninjaButton.style.opacity = '1';
+            ninjaButton.style.zIndex = '2'
+        }
+
+        //loggin google
+    const respuestaGoogle = async (respuesta)=>{
+        console.log(respuesta)
+        const login = {
+            apellido: respuestaGoogle.profileObj.familyName,
+            password: respuesta.profileObj.googleId,
+            email: respuesta.profileObj.email,
+            nombre: respuesta.profileObj.nombre,
+
+        }
+        console.log(login)
+        const a = await dispatch(newUser(login))
+        window.localStorage.setItem("token", a.token)
+    }
+    
 
     return (
-        <div className={`modal ${isOpen && 'modal-open'}`}>
+        <div id="regisModal" className="logModal">
             <div className="modal_dialog">
-            <button className="close" onClick={closeModal}>X</button>
             <form  className="formLogin" onSubmit={enviarInput}>
-                    <h1 className="loginUser">Nombre</h1>
-                    <input  placeholder="username" name="nombre" onChange={handleInputChange} />
-                    <h1 className="loginUser">Apellido</h1>
-                    <input  placeholder="apellido" name="apellido" onChange={handleInputChange} />
-                    <h1 className="loginPass">Contraseña</h1>
-                    <input placeholder="password" name="password" type="password" onChange={handleInputChange}/>
-                    <h1 className="loginPass">Correo electronico</h1>
+                    <h1 className="regisName">Nombre</h1>
+                    <input  placeholder="Tu nombre..." name="nombre" onChange={handleInputChange} />
+                    <h1 className="regisLastName">Apellido</h1>
+                    <input  placeholder="Tu apellido..." name="apellido" onChange={handleInputChange} />
+                    <h1 className="regisPass">Contraseña</h1>
+                    <input placeholder="Elige una contraseña..." name="password" type="password" onChange={handleInputChange}/>
+                    <h1 className="regisEmail">Correo electronico</h1>
                     <input placeholder="email" name="email" type="email" onChange={handleInputChange}/>
-                    <input id="buttonInput" type="submit" className="logginBtn" value ="Registrate" />
+                    <input id="buttonInput" type="submit" className="regisBtn" value ="Registrate" />
                 </form>
+                <GoogleLogin
+                    clientId="1306055516-vqakgi1c0sql95der98ul0vpsufbppd9.apps.googleusercontent.com"
+                    buttonText="Registrate con google"
+                    onSuccess={respuestaGoogle}
+                    onFailure={respuestaGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+                <button className="close" onClick={closeRegisModal}>X</button>
         </div>
         </div>
     )
