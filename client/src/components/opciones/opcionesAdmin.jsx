f166fe193e3eff3768d8c5826340799ff421789e
= <matiassundblad45@hotmail.com>
2021-09-01 13:07:00 -0300

    estilos nuevos

--- client/src/components/opciones/opcionesAdmin.jsx
+++ client/src/components/opciones/opcionesAdmin.jsx
@@ -8,9 +8,9 @@ export default function OpcionesAdmin (){
         <div className="opcionesAdmin">
             <h1>Bienvenido Admin</h1>
              <Link to='/profiles' style={{textDecoration:'none'}}><p className="perfiles">Perfiles de Usuarios</p></Link>
-             <Link to='/ordenes' style={{textDecoration:'none'}}><p className="ordenes">Ordenes de Compras</p></Link>
+             <Link to='/ordenes' style={{textDecoration:'none'}}><p className="ordenes">Órdenes de Compras</p></Link>
              <Link to='/add' style={{textDecoration:'none'}}><p className="agregar">Agregar Producto</p></Link>
-             <Link to='/categorias' style={{textDecoration:'none'}}><p className="categorias">Categorias</p></Link>
+             <Link to='/categorias' style={{textDecoration:'none'}}><p className="categorias">Categorías</p></Link>
         </div>
     )
 }
