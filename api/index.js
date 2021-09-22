f6a7ecae0fdeba11ecde5950bb37b402db00b636
= <matiassundblad45@hotmail.com>
2021-09-22 10:29:12 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use("/", routes);
 
 //escuchar peticiones
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log("Servidor corriento en el puerto 4000");
 });
--- api/src/configDB/config.js
+++ api/src/configDB/config.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
-const {DB_CNN}=process.env
+const { DB_CNN } = process.env;
 
-const dbConnection = async()=>{
-    try {
-        await mongoose.connect(DB_CNN,{
-            userNewUrlParser: true, 
-            useUnifiedTopology:true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('database connect')
-        
-    } catch (error) {
-        console.log(error);
-        throw new Error ('error a la hora de conectar')
-    }
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || DB_CNN, {
+      userNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("database connect");
+  } catch (error) {
+    console.log(error);
+    throw new Error("error a la hora de conectar");
+  }
+};
+
+if (proces.env.NODE_ENV === "production") {
+  app.use(express.static("../../client/build"));
 }
 
-module.exports ={
-    dbConnection
-}
+module.exports = {
+  dbConnection,
+};
